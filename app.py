a6c06f0dbd79d0bad79f04846392f10819e683c7
jake-hong <hongse21@gmail.com>
2021-11-06 23:51:03 +0900

    issue #3: Create follow/unfollow API

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, request,jsonify
+from flask.json import JSONEncoder
+
+#  Default JSON encoder 는 set을 JSON으로 변환할 수 없음. 
+# 커스텀 인코더를 작성하여 set을 list로 변환하고, jSON으로 변환 가능하게 해준다. 
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, object) :
+        if isinstance(object,set):
+            return list(object)
+        return JSONEncoder.default(self,object)
 
 app = Flask(__name__)
 app.users = {}
-app.id_count = 1 
+app.id_count = 1
+app.json_encoder =CustomJSONEncoder
 
-# 로그인 
+# 로그인
 @app.route('/sign_up', methods = ['POST'])
 def sign_up():
-    new_user = request.json 
+    new_user = request.json
     new_user['id'] = app.id_count
     app.users[app.id_count] = new_user
-    app.id_count = app.id_count + 1 
+    app.id_count = app.id_count + 1
 
     return jsonify(new_user)
 
@@ -25,13 +35,41 @@ def tweet():
 
     if user_id not in app.users:
         return '사용자가 존재하지 않습니다', 400
-    
+
     if len(tweet) > 300:
         return '300자가 초과했습니다',400
-    
+
     user_id = int(payload['id'])
     app.tweets.append({
         'user_id' : user_id,
         'tweet' : tweet
     })
     return '트윗이 작성되었습니다',200
+
+@app.route('/follow', methods = ['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+    
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    
+    user = app.users[user_id]
+    user.setdefault('follow',set()).add(user_id_to_follow)
+
+    return jsonify(user) 
+
+@app.route('/unfollow',methods = ['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['unfollow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow',set()).discard(user_id_to_follow)
+
+    return jsonify(user)
