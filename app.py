2b82c47b10125d6f9de486e68b221e67e4377095
jake-hong <hongse21@gmail.com>
2021-11-06 14:19:59 +0900

    issue#1: Create tweet API

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 app.users = {}
 app.id_count = 1 
 
-@app.route('/signup', methods = ['POST'])
+# 로그인 
+@app.route('/sign_up', methods = ['POST'])
 def sign_up():
     new_user = request.json 
     new_user['id'] = app.id_count
@@ -12,3 +13,25 @@ def sign_up():
     app.id_count = app.id_count + 1 
 
     return jsonify(new_user)
+
+# 300자 글 올리기
+app.tweets = []
+
+@app.route('/tweet', methods = ['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    
+    if len(tweet) > 300:
+        return '300자가 초과했습니다',400
+    
+    user_id = int(payload['id'])
+    app.tweets.append({
+        'user_id' : user_id,
+        'tweet' : tweet
+    })
+    return '트윗이 작성되었습니다',200
