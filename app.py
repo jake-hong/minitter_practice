faa3da25a446f31445f772b4a5ebacc9783948c9
jake-hong <hongse21@gmail.com>
2021-11-06 12:40:24 +0900

    first commit and create signup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request,jsonify
+
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1 
+
+@app.route('/signup', methods = ['POST'])
+def sign_up():
+    new_user = request.json 
+    new_user['id'] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1 
+
+    return jsonify(new_user)
